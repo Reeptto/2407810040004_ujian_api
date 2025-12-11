855498a6885dcbed552e7a6a78aaf601260e205c
Reeptto <0204ariaprianto@gmail.com>
2025-12-11 19:13:17 +0700

    Belum Apa2

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:ari_2407810040004_ujian_api/page/register.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // routes: {
+      //   "/" :(context)=>Page1(),
+      //   "/halaman2":(context)=>Page2()
+      // },
+      // initialRoute: "/",
+      home: RegisterPage()
+    );
+  }
+}
